e2d8961e78ce3e071e97ce9848977eb6090b13f1
Yaar Podshipnik <YaarPodshipnik@users.noreply.github.com>
2020-03-10 20:13:03 +0000

    Fixes for March 2020 tick rules (#1)
    * Update logic for March 2020 rule change
    
    Per https://www.eveonline.com/article/q6z2qy/patch-notes-for-march-2020-release:
    * When creating/updating a market order, the price will now always be rounded to a tick level with a maximum of 4 significant figures
    
    * Update README.md

--- eve-bump-pricer.py
+++ eve-bump-pricer.py
@@ -1,8 +1,11 @@
 import pyperclip
+from math import log10, floor
 from time import sleep
+
 """ 
 A tiny, simple, script to monitor cliboard for EVE online orders,
-calculate new +/-0.01 price, and put it back to clipboard.
+calculate new price accodring to March 2020 tick rules, and 
+put it back to clipboard.
 Helps dealing with updating multiple orders (no EULA-breaking
 automation).
 """
@@ -21,10 +24,28 @@ def get_order():
     else:
         return None, None
 
+def bump_price(price, direction = 1):
+    if price < 0:
+        raise ValueError("price can't be negative")
+    if direction not in [1, -1]:
+        raise ValueError(
+            "direction must be either 1 (increase price) or -1 (decrease price)")
+    most_significant = float(str(price)[:4])
+    mag_most_significant = floor(log10(most_significant))
+    mag_input = floor(log10(abs(price)))
+    mag_bump = -2 if mag_input < 3 else (mag_input - mag_most_significant)
+    bump  = 10 ** mag_bump 
+    bumped = price + (direction * bump)
+    if mag_input < 3:
+        return bumped
+    else:
+        mag_round = mag_most_significant - mag_input
+        return round(bumped, mag_round)
+    
 def get_updated_price(order, order_type):
     current_price = float(order[2].replace(" ISK", "").replace(",", ""))
-    adjustment = +0.01 if order_type == "BUY" else -0.01
-    new_price = current_price + adjustment
+    direction = +1 if order_type == "BUY" else -1
+    new_price = bump_price(current_price, direction)
     new_price = f"{new_price:.2f}"
     print(f"{order_type} order, old price {current_price}, new price {new_price}")
     return new_price
